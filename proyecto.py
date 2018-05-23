f5c3a37801db681836f51d56ef14a0ffbab9746d
oscarcorres <ocorres@gmail.com>
2018-05-23 09:32:36 +0200

    Initial commit

---
+++ proyecto.py
@@ -0,0 +1,13 @@
+# Proyecto final de Curso
+#
+#     Crea un nuevo proyecto para estos ejercicios
+#     Crea un nuevo fichero proyecto.py
+#     Descarga dos tipos de imágenes para poder alimentar la clasificación del proyecto,
+#       utilizando la extensión de Chrome: Fatkun Batch Descargar imagen
+#     Coloca las imágenes en el directorio images en el raiz del proyecto
+#     Dentro del proyecto.py incluye el código necesario para entrenar un modelo que permita
+#       la clasificación de imágenes
+#     Después del entrenamiento pasa una imagen al modelo para verificar si cataloga
+#       correctamente la nueva imagen
+#
+# Nota: Utilizar de base el ejemplo el 06_07 y el 06_08
